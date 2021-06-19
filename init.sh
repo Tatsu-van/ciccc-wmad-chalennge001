c3caddddfc7b856344cce36af9be15c79070980b
Tatsu-van <tatsuma.van@gmail.com>
2021-06-18 20:24:38 -0700

    first commit

---
+++ init.sh
@@ -0,0 +1,8 @@
+echo "# ciccc-wmad-chalennge001" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Tatsu-van/ciccc-wmad-chalennge001.git
+git push -u origin main
+
